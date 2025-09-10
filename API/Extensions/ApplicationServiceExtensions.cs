066f52ecda695971c8fc8b7f01ac9cc613030eca
giorgospikios <81362763+giorgospikios@users.noreply.github.com>
2025-09-10 15:58:30 +0300

    creating the account controller/service/middleware

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿// namespace API.Extensions;
+
+// public static class ApplicationServiceExtensions
+// {
+//     public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+//         IConfiguration config)
+//     {
+//         {
+            
+//         }
+//     }
+// }
